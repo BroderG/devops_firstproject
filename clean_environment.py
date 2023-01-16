e16d15bf32a2aeeaaecbf3160fc24025b058b0b2
BroderG <broder.guy@gmail.com>
2023-01-16 20:14:30 +0200

    Fixed method

--- clean_environment.py
+++ clean_environment.py
@@ -1,9 +1,10 @@
-import request
+import requests
+
 try:
   requests.get('http://127.0.0.1:5000/stop_server')
 except:
-  return ''
+  print("Rest_app server not responding")
 try:
   requests.get('http://127.0.0.1:5001/stop_server')
 except:
-  return ''
+  print("Web_app server not responding")
